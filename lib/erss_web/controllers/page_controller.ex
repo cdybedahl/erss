d51ea6105bc350aa4a885eb97cf62b2dbbb52f40
Calle Dybedahl <github@cyberpomo.com>
2020-01-26 20:09:34 +0100

    List all fandoms.

--- lib/erss_web/controllers/page_controller.ex
+++ lib/erss_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule ErssWeb.PageController do
   use ErssWeb, :controller
+  import Ecto.Query
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    fandoms =
+      from(f in Erss.Tag.Fandom, order_by: f.name)
+      |> Erss.Repo.all()
+      |> Enum.map(fn e -> %{name: e.name, url: e.url} end)
+
+    render(conn, "index.html", fandoms: fandoms)
   end
 end
