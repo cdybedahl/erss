161d16be5121fbd20dbffa9093fd92a37d34b902
Calle Dybedahl <calle@dybedahl.se>
2020-01-27 16:26:43 +0100

    Add a box with info on the most recent collected fic.

--- lib/erss_web/controllers/page_controller.ex
+++ lib/erss_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule ErssWeb.PageController do
   use ErssWeb, :controller
+  alias Erss.Repo
+  import Ecto.Query
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    fic =
+    from(f in Erss.Fic, order_by: [desc: :id], limit: 1)
+    |> Repo.one()
+    render(conn, "index.html", fic: fic)
   end
 end
