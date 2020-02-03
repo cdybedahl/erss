b239a425f5b2f164274f60dd43e0cb3b3cfb60d8
Calle Dybedahl <calle@dybedahl.se>
2020-02-03 10:28:35 +0100

    Redo ficlist.

--- lib/erss_web/controllers/fic_controller.ex
+++ lib/erss_web/controllers/fic_controller.ex
@@ -1,28 +1,31 @@
 defmodule ErssWeb.FicController do
   use ErssWeb, :controller
 
+  @pagelen 25
+
   import Ecto.Query
   alias Erss.Repo
 
   def index(conn, %{"page" => page}) do
     p = String.to_integer(page)
+    max = Integer.floor_div(Repo.aggregate(Erss.Fic, :count), @pagelen) + 1
 
     fics =
       from(f in Erss.Fic,
         order_by: [desc: f.id],
-        limit: 25,
-        preload: :fandoms,
-        offset: 25 * ^p
+        limit: @pagelen,
+        preload: [:fandoms, :relationships],
+        offset: @pagelen * (^p - 1)
       )
       |> Repo.all()
       |> Enum.map(fn f -> Map.put(f, :total, Erss.Fic.total_rating(f)) end)
       |> Enum.map(&rating_class/1)
 
-    render(conn, "index.html", fics: fics)
+    render(conn, "index.html", fics: fics, page: p, max_page: max, len: @pagelen)
   end
 
   def index(conn, params) do
-    index(conn, Map.put(params, "page", "0"))
+    index(conn, Map.put(params, "page", "1"))
   end
 
   defp rating_class(fic = %{:total => total}) when total < 0 do
