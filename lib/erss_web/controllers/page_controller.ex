4036b56ed217a8f16d0871d277c91c534820932b
Calle Dybedahl <github@cyberpomo.com>
2020-02-07 20:28:18 +0100

    Basic table of fics.

--- lib/erss_web/controllers/page_controller.ex
+++ lib/erss_web/controllers/page_controller.ex
@@ -1,7 +1,28 @@
 defmodule ErssWeb.PageController do
   use ErssWeb, :controller
 
+  import Ecto.Query
+  alias Erss.Repo
+
   def index(conn, _params) do
-    render(conn, "index.html", fics: Erss.Fic.all())
+    fics =
+      from(f in Erss.Fic, preload: [:author, :rating, :categories])
+      |> Repo.all()
+      |> Enum.map(fn f -> Map.put(f, :total, rating_class(Erss.Fic.total_rating(f))) end)
+
+    render(conn, "index.html", fics: fics)
+  end
+
+  defp rating_class(n) do
+    cond do
+      n > 2 ->
+        "good"
+
+      n < 0 ->
+        "bad"
+
+      true ->
+        "neutral"
+    end
   end
 end
