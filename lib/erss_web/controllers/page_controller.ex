9b9c8fa065ff89caaebab230b2db5b50d4687b1d
Calle Dybedahl <github@cyberpomo.com>
2020-02-07 18:45:50 +0100

    Clean:ish start to web stuff.

--- lib/erss_web/controllers/page_controller.ex
+++ lib/erss_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule ErssWeb.PageController do
   use ErssWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", fics: Erss.Fic.all())
   end
 end
