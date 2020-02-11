d3e5fa55ee6c32faf00f63ce6fae3245ae1c7107
Calle Dybedahl <github@cyberpomo.com>
2020-02-11 20:55:14 +0100

    Clean out dead code.

--- lib/erss/tag.ex
+++ lib/erss/tag.ex
@@ -2,7 +2,6 @@ defmodule Erss.Tag do
   use Ecto.Schema
 
   alias Erss.Repo
-  import Ecto.Query
 
   schema "tag" do
     field(:url, :string, null: false)
@@ -36,79 +35,5 @@ defmodule Erss.Tag do
 
   def get_tagset(list) do
     Enum.map(list, &get_or_insert/1)
-  end
-
-  def all_authors do
-    from(t in Erss.Tag, join: f in Erss.Fic, on: f.author_id == t.id, select: t, distinct: true)
-  end
-
-  def all_authors_with_counts do
-    from(t in Erss.Tag,
-      join: f in Erss.Fic,
-      on: f.author_id == t.id,
-      group_by: t.id,
-      select: {t, count(t.id)},
-      order_by: [desc: count(t.id)]
-    )
-  end
-
-  def all_ratings do
-    from(t in Erss.Tag, join: f in Erss.Fic, on: f.rating_id == t.id, select: t, distinct: true)
-  end
-
-  def all_ratings_with_counts do
-    from(t in Erss.Tag,
-      join: f in Erss.Fic,
-      on: f.rating_id == t.id,
-      group_by: t.id,
-      select: {t, count(t.id)},
-      order_by: [desc: count(t.id)]
-    )
-  end
-
-  def all_languages do
-    from(t in Erss.Tag, join: f in Erss.Fic, on: f.language_id == t.id, select: t, distinct: true)
-  end
-
-  def all_languages_with_counts do
-    from(t in Erss.Tag,
-      join: f in Erss.Fic,
-      on: f.language_id == t.id,
-      group_by: t.id,
-      select: {t, count(t.id)},
-      order_by: [desc: count(t.id)]
-    )
-  end
-
-  def all_warnings, do: all_from_table("warning_fic")
-  def all_warnings_with_counts, do: all_from_table_with_counts("warning_fic")
-
-  def all_relationships, do: all_from_table("relationship_fic")
-  def all_relationships_with_counts, do: all_from_table_with_counts("relationship_fic")
-
-  def all_fandoms, do: all_from_table("fandom_fic")
-  def all_fandoms_with_counts, do: all_from_table_with_counts("fandom_fic")
-
-  def all_characters, do: all_from_table("character_fic")
-  def all_characters_with_counts, do: all_from_table_with_counts("character_fic")
-
-  def all_categories, do: all_from_table("category_fic")
-  def all_categories_with_counts, do: all_from_table_with_counts("category_fic")
-
-  def all_additional_tags, do: all_from_table("additional_fic")
-  def all_additional_tags_with_counts, do: all_from_table_with_counts("additional_fic")
-
-  def all_from_table(table) do
-    from(t in Erss.Tag, join: wf in ^table, on: wf.tag_id == t.id, distinct: true)
-  end
-
-  def all_from_table_with_counts(table) do
-    from(t in Erss.Tag,
-      join: wf in ^table,
-      on: wf.tag_id == t.id,
-      group_by: t.id,
-      select: {t, count(t.id)},
-      order_by: [desc: count(t.id)]
-    )
   end
 end
