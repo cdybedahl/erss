601dff94c559a9d0bf5e5ab9a55c59ae485a2ba9
Calle Dybedahl <github@calle.dybedahl.se>
2020-02-22 22:03:32 +0100

    Bugfix author, rating and language tag lists.

--- foo.sql
+++ foo.sql
@@ -1,11 +1,9 @@
-CREATE OR REPLACE VIEW tag_user_rating AS
-SELECT i.tag_id AS tag_id,
-       i.user_id AS user_id,
-       COALESCE(r.rating,0) AS rating
-FROM
-    (SELECT tag.id AS tag_id,
-            users.id AS user_id
-     FROM tag
-     JOIN users ON TRUE) AS i
-LEFT JOIN rating AS r ON r.tag_id = i.tag_id
-AND r.user_id = i.user_id
+SELECT t.name,
+       tur.rating,
+       count(f.id)
+FROM tag_user_rating AS tur
+JOIN fic AS f ON f.language_id = tur.tag_id
+JOIN tag AS t ON tur.tag_id = t.id
+WHERE tur.user_id = 1
+GROUP BY t.name,
+         tur.rating
--- lib/erss_web/controllers/tag_controller.ex
+++ lib/erss_web/controllers/tag_controller.ex
@@ -23,23 +23,18 @@ defmodule ErssWeb.TagController do
     render(conn, "index.html", tags: tags, maxpage: maxpage, type: type, page: page)
   end
 
-  def source(type, user_id) do
-    table =
-      Map.get(
-        %{
-          "author" => "author_fic",
-          "fandom" => "fandom_fic",
-          "additional" => "additional_fic",
-          "category" => "category_fic",
-          "character" => "character_fic",
-          "language" => "language_fic",
-          "rating" => "rating_fic",
-          "relationship" => "relationship_fic",
-          "warning" => "warning_fic"
-        },
-        type
-      )
+  def source("fandom", user), do: table_source("fandom_fic", user)
+  def source("additional", user), do: table_source("additional_fic", user)
+  def source("character", user), do: table_source("character_fic", user)
+  def source("category", user), do: table_source("category_fic", user)
+  def source("relationship", user), do: table_source("relationship_fic", user)
+  def source("warning", user), do: table_source("warning_fic", user)
 
+  def source("author", user), do: fic_source("author_id", user)
+  def source("rating", user), do: fic_source("rating_id", user)
+  def source("language", user), do: fic_source("language_id", user)
+
+  def table_source(table, user_id) do
     q1 =
       from(tt in table,
         join: tur in "tag_user_rating",
@@ -48,6 +43,50 @@ defmodule ErssWeb.TagController do
         select: %{tag_id: tt.tag_id, rating: tur.rating, count: count(tt.tag_id)}
       )
 
+    common_source(q1)
+  end
+
+  # The next three repeat a lot of code. It must be possible to fix.
+  def fic_source("rating_id", user_id) do
+    q1 =
+      from(tur in "tag_user_rating",
+        join: f in Erss.Fic,
+        on: tur.tag_id == f.rating_id,
+        where: tur.user_id == ^user_id,
+        group_by: [tur.tag_id, tur.rating],
+        select: %{tag_id: tur.tag_id, rating: tur.rating, count: count([tur.tag_id, tur.rating])}
+      )
+
+    common_source(q1)
+  end
+
+  def fic_source("author_id", user_id) do
+    q1 =
+      from(tur in "tag_user_rating",
+        join: f in Erss.Fic,
+        on: tur.tag_id == f.author_id,
+        where: tur.user_id == ^user_id,
+        group_by: [tur.tag_id, tur.rating],
+        select: %{tag_id: tur.tag_id, rating: tur.rating, count: count([tur.tag_id, tur.rating])}
+      )
+
+    common_source(q1)
+  end
+
+  def fic_source("language_id", user_id) do
+    q1 =
+      from(tur in "tag_user_rating",
+        join: f in Erss.Fic,
+        on: tur.tag_id == f.language_id,
+        where: tur.user_id == ^user_id,
+        group_by: [tur.tag_id, tur.rating],
+        select: %{tag_id: tur.tag_id, rating: tur.rating, count: count([tur.tag_id, tur.rating])}
+      )
+
+    common_source(q1)
+  end
+
+  def common_source(q1) do
     from(f in subquery(q1),
       join: t in Erss.Tag,
       on: f.tag_id == t.id,
